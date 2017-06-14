0819d1fb72ed8033f8f57e532bbd3c1bbaf6c4e8
ngocsonqs <ngocson_jp@yahoo.co.jp>
2017-06-14 20:42:34 +0900

    PROMISE - 04. ví dụ cơ bản về reject

--- public/promise/01-promise-basic.js
+++ public/promise/01-promise-basic.js
@@ -17,3 +17,18 @@ let pr3 = new Promise( (resolve, reject) => {
     resolve("success");
 });
 pr3.then( (value) => console.log("Trạng thái ", value));
+
+
+//////////////////////////////////////////////////////
+// ④． ví dụ về reject( có lỗi xảy ra)              //
+//////////////////////////////////////////////////////
+let pr4 = new Promise( (resolve, reject) => {
+    reject( new Error("Fail"));
+});
+pr4.then(
+    // TRƯỜNG HỢP resolve(thành công) thì sẽ chạy câu lệnh này
+    (msg) => console.log("Trạng thái ", msg),
+
+    // TRƯỜNG HỢP reject(có lỗi) thì sẽ chạy câu lệnh này
+    (err) => console.log("Trạng thái ", err + "")
+);
