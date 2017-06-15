f64e5c4dd11432301d78fd65fd129dc79bb3d69d
ngocsonqs <ngocson_jp@yahoo.co.jp>
2017-06-16 02:10:05 +0900

    PROMISE CHAIN - 01. vd về phép cộng 2 số

---
+++ public/promise/03-promise-lientiep.js
@@ -0,0 +1,19 @@
+let add = ( num1, num2 ) => {
+    return new Promise( (resolve, reject) => {
+        if ( typeof num1 != 'number' || typeof num2 != 'number') {
+            return reject( new Error("Tham số truyền vào phải là kiểu số"));
+        }
+        return resolve( num1 + num2 );
+    });
+};
+
+// thực thi 4 + 5 + 6 = 15
+add("4", 5)
+    .then(
+        result => add(result, 6),
+        errMsg => console.log(errMsg + "")
+    )
+    .then(
+        result => console.log(result),
+        errMsg => console.log(errMsg + "")
+    )
